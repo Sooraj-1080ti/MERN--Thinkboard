7450a2ec32a53068afec5117276e8dcb9884b8d8
Sooraj <soorajsuresh.in@gmail.com>
2026-06-23 17:27:11 +0530

    deployment prep2

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 //in production we will use the production url and in development we will use localhost url
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "https://your-production-url.com/api";
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
 const api = axios.create({
     baseURL: BASE_URL,
 });
